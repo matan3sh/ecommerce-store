0e2409b7daa6657d8cdc41bfb78fc90844e11f77
Matan <matan3sh@gmail.com>
2024-05-03 20:07:59 +0300

    Start Implementing ProductCard Component

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -1,3 +1,5 @@
+import NoResults from '@/components/ui/NoResults'
+import ProductCard from '@/components/ui/ProductCard'
 import { Product } from '@/types'
 import React from 'react'
 
@@ -9,7 +11,13 @@ interface ProductListProps {
 const ProductList: React.FC<ProductListProps> = ({ items, title }) => {
   return (
     <div className="space-y-4">
-      <h3>{title}</h3>
+      <h3 className="font-bold text-3xl">{title}</h3>
+      {items.length === 0 && <NoResults />}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {items.map((item) => (
+          <ProductCard key={item.id} data={item} />
+        ))}
+      </div>
     </div>
   )
 }
