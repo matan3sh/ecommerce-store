7f4e87cc632ab9209c53c43695ce8cca70697c31
Matan <matan3sh@gmail.com>
2024-05-07 18:50:39 +0300

    DIsplaying Products

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -5,6 +5,8 @@ import getSizes from '@/actions/get-sizes'
 import Filter from '@/app/(routes)/category/[categoryId]/components/Filter'
 import Billboard from '@/components/Billboard'
 import Container from '@/components/ui/Container'
+import NoResults from '@/components/ui/NoResults'
+import ProductCard from '@/components/ui/ProductCard'
 
 export const revalidate = 0
 
@@ -41,6 +43,14 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
             <div className="hidden lg:block">
               <Filter valueKey="sizeId" name="Sizes" data={sizes} />
               <Filter valueKey="colorId" name="Colors" data={colors} />
+            </div>
+            <div className="mt-6 lg:col-span-4 lg:mt-0">
+              {products.length === 0 && <NoResults />}
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                {products.map((product) => (
+                  <ProductCard key={product.id} data={product} />
+                ))}
+              </div>
             </div>
           </div>
         </div>
