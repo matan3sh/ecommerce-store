030e0799aa2e82f724a6e64bb203976d29ac0fb2
Matan <matan3sh@gmail.com>
2024-05-08 09:47:06 +0300

    UseCart Implementation

---
+++ hooks/use-cart.ts
@@ -0,0 +1,46 @@
+import { Product } from '@/types'
+import toast from 'react-hot-toast'
+import { create } from 'zustand'
+import { createJSONStorage, persist } from 'zustand/middleware'
+
+interface CartStore {
+  items: Product[]
+  addItem: (data: Product) => void
+  removeItem: (id: string) => void
+  removeAll: () => void
+}
+
+const useCart = create(
+  persist<CartStore>(
+    (set, get) => ({
+      items: [],
+      addItem: (data) => {
+        const currentItems = get().items
+        const existingItem = currentItems.find((item) => item.id === data.id)
+
+        if (existingItem) {
+          return toast('Item already in cart.')
+        }
+
+        set({ items: [...currentItems, data] })
+        toast.success('Item added to cart.')
+      },
+      removeItem: (id) => {
+        const currentItems = get().items
+        const newItems = currentItems.filter((item) => item.id !== id)
+        set({ items: newItems })
+        toast.success('Item removed from cart.')
+      },
+      removeAll: () => {
+        set({ items: [] })
+        toast.success('Cart cleared.')
+      },
+    }),
+    {
+      name: 'cart-storage',
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+)
+
+export default useCart
